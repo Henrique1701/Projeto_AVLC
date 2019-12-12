fb59207049acf982d7af20d2d85ed39a04afec7d
José Henrique Fernandes Silva <48698947+Henrique1701@users.noreply.github.com>
2019-12-12 16:18:55 -0300

    Update Matriz.java

--- Matriz.java
+++ Matriz.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 public class Matrizes {
 
     public static void main(String[] args) {
-// TODO Auto-generated method stub
         Scanner in = new Scanner(System.in);
         int tamMatriz = 0;
         System.out.println("Digite o tamanho da matriz:");
@@ -18,22 +17,38 @@ public class Matrizes {
                 c [w][x] = 0;
             }
         }
-        
-        
-        for (int i = 0; i < tamMatriz; i++) {
-            for (int j = 0; j < tamMatriz; j++) {
-                for (int k = 0 ; k < tamMatriz; k++) {
-                    c [i] [j] = c [i] [j] + (a [i][k]*b [k][j]);
+
+        int potencia = 2, controle = 1;
+        while (controle == 1){
+            for (int i = 0; i < tamMatriz; i++) {
+                for (int j = 0; j < tamMatriz; j++) {
+                    for (int k = 0 ; k < tamMatriz; k++) {
+                        c [i] [j] = c [i] [j] + (a [i][k]*b [k][j]);
+                    }
                 }
             }
-        }
 
-        System.out.println("Sou matriz de adjacência ^2");
-        for (int w = 0; w < 4;w++) {
-            for (int x = 0; x < 4; x++) {
-                System.out.print(c [w][x] + " ");
+            System.out.println("Sou matriz de adjacência ^"+ potencia);
+            for (int w = 0; w < 4;w++) {
+                for (int x = 0; x < tamMatriz; x++) {
+                    System.out.print(c [w][x] + " ");
+                }
+                System.out.println();
             }
+
             System.out.println();
+            System.out.println("Deseja ver a próxima potência ?  [1=Sim/0=Não]");
+            controle = in.nextInt();
+            potencia++;
+
+            if(controle == 1){
+                for (int w = 0; w < tamMatriz;w++) {
+                    for (int x = 0; x < tamMatriz; x++) {
+                        b [w] [x] = c [w] [x];
+                        c [w][x] = 0;
+                    }
+                }
+            }
         }
     }
 
